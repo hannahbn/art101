d427fca0c8a00c8967e4f4abbaeec3f45cbff139
HannahBN1 <48965725+hannahbn@users.noreply.github.com>
2023-10-20 15:56:20 -0700

    Lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -19,3 +19,37 @@ function main() {
 
 // let's get this party started
 main();
+// Pseudocode everyday task: Banana Bread Recipe
+// Evaluate when the bananas are spoiled/over ripened. 
+// If ripe, prepare ingredients and materials needed.
+// In a large mixing bowl combine: cream, butter, sugar, eggs, and mashed bananas.
+// In a separate bowl whisk together dry ingredients: flour, salt, and baking soda.
+// Combine wet and dry ingredients; add chocolate chips, raisins or walnuts. 
+// Pour mixture into loaf pan. 
+// Bake loaf.
+// Wait for bread to cool before cutting for best cut. 
+// Enjoy bread.  
+
+
+// Pseudocode high level list: BEER PONG
+// Initialize the Game:
+  // Create a game board with cups arranged in a triangle formation at each end.
+  // Set up two teams or players.
+  // Initialize game state variables, including scores and game mode.
+// Start the Game Loop:
+  //Create a loop that continues until the game is over.
+// Render the Game Board:
+  // Display the game board with cups, and possibly the ball's trajectory.
+// Player Input:
+  // Listen for player input to aim and throw the ball.
+// Calculate the Ball's Trajectory:
+  // Determine the path and motion of the ball based on player input and physics.
+// Check for Ball-Cup Collisions:
+  // Detect if the ball collides with any of the cups.
+  // Remove the hit cup if a collision occurs, and update the score.
+// Check for Game Over:
+  // Check if one team reaches the winning score or if the game should end for other reasons.
+// End Game:
+  // Display a message indicating the winning team.
+  // Ask the players if they want to play again.
+  // (ChatGPT used for the above pseudocode)
