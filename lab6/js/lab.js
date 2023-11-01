f4dfa55185e7130b84f33e6cced1e06fe8677774
HannahBN1 <48965725+hannahbn@users.noreply.github.com>
2023-11-01 16:08:22 -0700

    lab 7

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,26 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Arrays and Objects
+// Author: Hannah Nielsen
+// Date: 10/27/23
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
 
 // let's get this party started
-main();
+//Defining variables
+myTransport = ["legs", "bus", "bike", "2008 honda element with broken passenger door", ];
+
+// creating an object for my ride
+myMainRide = {
+  Make:"BMW",
+  Model:"325i",
+  Color: "black",
+  Year: 2005,
+  age:function(){
+    return 2023 - this.year;
+},
+};
+
+ //output
+document.writeln ("My Types of Transportation:", myTransport,"</br>");
+
+//how to write an object to the document
+document.writeln ("My Main Ride: <pre>",
+JSON.stringify(myMainRide, null, '\t'), "</pre>");
